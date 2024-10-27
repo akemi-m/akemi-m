bc0762aa013a58bcbb5165e12d63579f9173f5a1
akemi <akemi.mullis@gmail.com>
2024-10-27 18:31:31 -0300

    Adição da navbar e ajustes na timeline

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,24 @@ import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
 
+/* navbar */
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+/* navbar /*/
+
+/* 3d */
 let scene, camera, renderer, composer, sphere, redLight;
 let lightAngle = 0;
 
@@ -87,5 +105,6 @@ function animate() {
 
   composer.render();
 }
+/* 3d /*/
 
 init();
